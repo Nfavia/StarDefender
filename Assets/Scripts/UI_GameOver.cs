a5c67bb6a392f3b013bfd914cb100b51dd26e054
Nfavia <Nfavia1297@gmail.com>
2023-03-17 15:42:56 -0500

    Fixed build again

--- Assets/Scripts/UI_GameOver.cs
+++ Assets/Scripts/UI_GameOver.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UI_GameOver : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI finalScoreText;
+    [SerializeField] Button replayButton;
+    [SerializeField] Button menuButton;
 
     Scorekeeper scorekeeper;
+    LevelManager levelManager;
 
     private void Awake()
     {
-        scorekeeper = FindObjectOfType<Scorekeeper>();     
+        scorekeeper = FindObjectOfType<Scorekeeper>();
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
     void Start()
     {
-        if (scorekeeper == null)
+        if (!scorekeeper)
             Debug.LogError("Scorekeeper Object Not Found");
-        
+        if (!levelManager)
+            Debug.LogError("LevelManager Object Not Found");
+
+        ButtonSetup();
     }
 
     private void Update()
@@ -26,4 +34,10 @@ public class UI_GameOver : MonoBehaviour
         finalScoreText.text = "Your Score:\n" + scorekeeper.GetCurrentScore();
     }
 
+    // Set up on click listeners since they dont save the singleton LevelManager
+    private void ButtonSetup()
+    {
+        replayButton.onClick.AddListener(levelManager.LoadGane);
+        menuButton.onClick.AddListener(levelManager.LoadMainMenu);
+    }
 }
